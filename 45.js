702643ff50352d2a146d15973e9e85c4e0bc497e
kadropo <riahkik@gmail.com>
2025-11-22 08:09:57 +0100

    Create 45.js
    4554 scrilo lokh kmoi

---
+++ 45.js
@@ -0,0 +1,20 @@
+  
+
+<div id="Layer0" style="top: 145px; left: 70px; position: fixed; z-index: 0;">
+<iframe id="iframePlayer" name="iframePlayer" width="578" height="392" src="https://www.youtube.com/embed/Wpg6fEpKPGE?version=3&amp;hl=fr_FR&amp;showinfo=0&amp;rel=0&amp;autoplay=1" frameborder="0"></iframe> 
+
+</div> 
+
+
+<div id="Layer0" style="top: 129px; left: 736px; position: fixed; z-index: 0;">
+
+<iframe allowtransparency="true" frameborder="0" height="414" scrolling="oui" src="
+https://www.kadero.tn/p/s1.html" width="216"></iframe>
+
+
+
+
+
+
+
+</div>
